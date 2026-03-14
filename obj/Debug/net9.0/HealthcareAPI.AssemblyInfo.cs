653ded877d98b0f859a849570d4fef114ba9d597
Manish <your-email@gmail.com>
2026-03-14 17:46:09 +0530

    Cors added

--- obj/Debug/net9.0/HealthcareAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/HealthcareAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthcareAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32f8015c3c0454066e3ee7f0ec6480a2c20a1ce9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+546f27d8851f2acb98cb525dabaddf72110936c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthcareAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthcareAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
